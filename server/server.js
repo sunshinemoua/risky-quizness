2e5630dd168624966234a8de8eda5df9d7fe0283
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-09-19 15:02:14 -0700

    app listening on port 3001; connected to mongoDB

--- server/server.js
+++ server/server.js
@@ -2,5 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://sunshinemoua21:hello123@user.8ymipx7.mongodb.net/")
+  .then(() => {
+    console.log("connected to MongoDB");
+    app.listen(3001, (req, res) => {
+      console.log("listening on port 3001");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 console.log("hello db");
